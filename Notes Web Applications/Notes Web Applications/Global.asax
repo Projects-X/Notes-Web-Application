9807604889143d204de049ea9f62bbd4e093c91c
ankitkhanna.khanna90 <ankitkhanna.khanna90@gmail.com>
2017-03-23 12:19:07 +0530

    Initial Commit (Add Project)

---
+++ Notes Web Applications/Notes Web Applications/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Notes_Web_Applications.MvcApplication" Language="C#" %>
---
+++ Notes Web Applications/Notes Web Applications/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Notes_Web_Applications.Startup))]
+namespace Notes_Web_Applications
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
